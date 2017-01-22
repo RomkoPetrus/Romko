f2b1b2b9bcca8aaaaed7871631bfa4b36da20f07
RomkoPetrus <petrusrom@gmail.com>
2017-01-22 13:56:26 +0200

    Signed-off-by: RomkoPetrus <petrusrom@gmail.com>

---
+++ chapter_12/12_4.c
@@ -0,0 +1,27 @@
+/*
+Chapter 12 Exercise 4:
+
+Write and test in a loop a function
+that returns the number of times it has been called.
+*/
+
+#include <stdio.h>
+
+unsigned int function(void);
+
+int main (void){
+	int count;
+	puts("Enter number of loops: ");
+	scanf("%d", &count);
+	unsigned int n = 0;
+
+	for (int i = 0; i < count; ++i) {
+		n = function();
+	}
+	printf("Number of calls is %d\n", n);
+}
+
+unsigned int function(void){
+	static unsigned int times;
+	return ++times; 
+}
